40f7127a6e9c9b5591622d0c2a61fc89893a9ecb
Gabriel-06 <44278698+Gabriel-06@users.noreply.github.com>
2019-11-01 11:02:14 +1030

    Initial commit

---
+++ content.js
@@ -0,0 +1,172 @@
+const maintext = document.getElementsByTagName('div');
+for (elt of maintext) {
+  elt.style['color'] = '#FFF';
+}
+
+const card = document.getElementsByClassName('card');
+for (elt of card) {
+  elt.style['background-color'] = '#292929';
+  elt.style['border-width'] = '1px';
+  elt.style['border-color'] = '#535353';
+}
+
+const toolbar = document.getElementsByClassName('Toolbar');
+for (elt of toolbar) {
+  elt.style['background-color'] = '#3e3e3e';
+  elt.style['border-width'] = '1px';
+  elt.style['border-color'] = '#515151';
+}
+
+const timetable = document.getElementsByClassName('L ditm');
+for (elt of timetable) {
+  elt.style['background-color'] = '#3e3e3e';
+  elt.style['border-color'] = '#666666';
+}
+
+const msgunread = document.getElementsByClassName('msg unread');
+for (elt of msgunread) {
+  elt.style['background-color'] = '#3e3e3e';
+  elt.style['color'] = '#e5e5e5';
+}
+
+const msg = document.getElementsByClassName('msg');
+for (elt of msg) {
+  elt.style['background-color'] = '#3e3e3e';
+  elt.style['color'] = '#e5e5e5';
+  elt.style['border-color'] = '#515151';
+  elt.style['border-width'] = '1px';
+}
+
+const text = document.getElementsByTagName('a');
+for (elt of text) {
+  elt.style['color'] = '#e5e5e5';
+}
+
+const h2 = document.getElementsByTagName('h2');
+for (elt of h2) {
+  elt.style['color'] = '#e5e5e5';
+}
+
+const search = document.getElementsByClassName('hasDatepicker');
+for (elt of search) {
+  elt.style['background-color'] = '#e5e5e5';
+  elt.style['border-color'] = '#ccc';
+}
+
+const caption = document.getElementsByClassName('Caption');
+for (elt of caption) {
+  elt.style['color'] = '#e5e5e5';
+}
+
+const header = document.getElementsByClassName('Header');
+for (elt of header) {
+  elt.style['color'] = '#e5e5e5';
+  elt.style['background-color'] = '#292929';
+}
+
+const btn = document.getElementsByClassName('btn');
+for (elt of btn) {
+  elt.style['background-color'] = '#7e7e7e';
+}
+
+const sn = document.getElementsByClassName('sn');
+for (elt of sn) {
+  elt.style['color'] = '#acacac';
+}
+
+const tasks = document.getElementsByClassName('active itm');
+for (elt of tasks) {
+  elt.style['background-color'] = '#3e3e3e';
+  elt.style['border-width'] = '1px';
+  elt.style['border-color'] = '#515151';
+}
+
+const cap = document.getElementsByClassName('cap');
+for (elt of cap) {
+  elt.style['color'] = '#acacac';
+}
+
+const title = document.getElementsByClassName('lpTitle');
+for (elt of title) {
+  elt.style['color'] = '#FFF';
+}
+
+const postticker = document.getElementsByClassName('post-ticker');
+for (elt of postticker) {
+  elt.style['background-color'] = '#7e7e7e';
+  elt.style['boxShadow'] = 'none';
+}
+
+const diaryDay = document.getElementsByClassName('diaryDay');
+for (elt of diaryDay) {
+  elt.style['background-color'] = '#7e7e7e';
+}
+
+const headerr = document.getElementsByClassName('header');
+for (elt of headerr) {
+  elt.style['color'] = '#e5e5e5';
+  elt.style['background-color'] = '#292929';
+  elt.style['border-color'] = '#535353';
+}
+
+const menu = document.getElementById('mnu').getElementsByTagName('td');
+for (elt of menu) {
+    elt.style['background-color'] = 'transparent';
+    elt.style['border-bottom'] = '0px';
+    elt.style['color'] = '#FFF'
+}
+
+const body = document.getElementById('mainBody').style.backgroundColor = '#3e3e3e';
+
+const attendance = document.getElementsByClassName('sdIndicator');
+for (elt of attendance) {
+  elt.style['color'] = '#292929';
+}
+
+const mdi = document.getElementsByClassName('mdi mdi-menu');
+for (elt of mdi) {
+  elt.style['color'] = '#FFF';
+}
+
+const dlgcontent = document.getElementsByClassName('dlgContent');
+for (elt of dlgcontent) {
+  elt.style['background-color'] = '#292929';
+}
+
+const msgshadow = document.getElementById('btnMsgs').style.boxShadow = 'none';
+const diaryshadow = document.getElementById('btnDiary').style.boxShadow = 'none';
+const calendarshadows = document.getElementById('bCalendar').style.boxShadow = 'none';
+
+const taskfinder = document.getElementsByClassName('lpMenuTd2');
+for (elt of taskfinder) {
+  elt.style['border-left'] = '0px';
+}
+const menuactive = document.getElementsByClassName('lpMenuTop active');
+for (elt of menuactive) {
+  elt.style['border-left'] = '0px';
+}
+
+const logo = document.getElementsByClassName('logo')[2].src = 'https://i1383.photobucket.com/albums/ah310/SDFHwg/ezgif-2-24d01c8b07de_zpselqs9mpo.png';
+
+const captionr8 = document.getElementsByClassName('CaptionR8');
+for (elt of captionr8) {
+  elt.style['color'] = '#e5e5e5';
+}
+
+console.log(document.getElementsByClassName('gr__daymap_marryatvillehs_sa_edu_au'));
+
+const hypertext = document.getElementsByClassName('dlgContent');
+for (elt of hypertext) {
+  elt.style['background-color'] = '#292929';
+}
+
+const week = document.getElementsByClassName('CE ditm');
+for (elt of week) {
+  elt.style['background-color'] = '#3e3e3e';
+  elt.style['border-left-color'] = '#666666';
+}
+
+const name_font = document.getElementsByClassName('sname');
+for (elt of name_font) {
+  elt.style['font-family'] = "'Roboto', 'Helvetica Neue',Helvetica,Arial,sans-serif";
+}
