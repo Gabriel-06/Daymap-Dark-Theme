145d42d17bd008c623058bc2081d686db50cdbc4
Gabriel-06 <44278698+Gabriel-06@users.noreply.github.com>
2019-11-01 13:07:47 +1030

    Update content.js

--- content.js
+++ content.js
@@ -133,9 +133,9 @@ for (elt of dlgcontent) {
   elt.style['background-color'] = '#292929';
 }
 
-const msgshadow = document.getElementById('btnMsgs').style.boxShadow = 'none';
-const diaryshadow = document.getElementById('btnDiary').style.boxShadow = 'none';
-const calendarshadows = document.getElementById('bCalendar').style.boxShadow = 'none';
+//const msgshadow = document.getElementById('btnMsgs').style.boxShadow = 'none';
+//const diaryshadow = document.getElementById('btnDiary').style.boxShadow = 'none';
+//const calendarshadows = document.getElementById('bCalendar').style.boxShadow = 'none';
 
 const taskfinder = document.getElementsByClassName('lpMenuTd2');
 for (elt of taskfinder) {
@@ -177,4 +177,17 @@ for (elt of mobile_header) {
 
 const mobile_logo = document.getElementsByClassName('logo')[1].src = 'https://i.ibb.co/SR4c9fT/logo-trans.png';
 
-console.log(document.getElementsByClassName('logo'));
+console.log(document.getElementsByClassName('Period'));
+
+
+//calender stuff
+
+const period = document.getElementsByClassName('Period');
+for (elt of period) {
+  elt.style['backgroundColor'] = '#292929';
+}
+
+const planclass_bg = document.getElementsByClassName('PlanClass');
+for (elt of planclass_bg) {
+  elt.style['backgroundColor'] = '#292929';
+}
